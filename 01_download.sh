a7dd6cafd22aeae39346ad64f4771467494b8d8c
andrey <andrey4d.dev@gmail.com>
2023-01-22 19:35:01 +0700

    MOVE 03_create-network.sh to create-ipxe.sh CHANGE README.md

--- 01_download.sh
+++ 01_download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+VERSION="37.20221225.3.0"
 mkdir coreos
 podman run --security-opt label=disable --pull=always --rm -v $PWD/coreos:/data -w /data quay.io/coreos/coreos-installer:release download -f pxe

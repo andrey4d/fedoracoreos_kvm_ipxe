3035404c0fc40d01610753e9947c87bbbdddcaff
andrey4d <andrey4d.dev@gmail.com>
2023-11-20 23:04:24 +0700

    Changes

--- create-ipxe.sh
+++ create-ipxe.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-DOCKER="sudo podman"
+DOCKER="docker"
 #IMAGE_BUTANE="quay.io/coreos/butane:release"
 IMAGE_BUTANE="quay.io/coreos/fcct:latest"
 
-VERSION="37.20221225.3.0"
+VERSION="38.20231027.3.1"
 IMAGE_DOWNLOAD="quay.io/coreos/coreos-installer:release"
+
+CONNECTION="qemu:///session?socket=/Users/andrey/.cache/libvirt/libvirt-sock"
 
 
 ${DOCKER} run --security-opt label=disable --pull=always --rm -v "${PWD}/coreos:/data" -w /data ${IMAGE_DOWNLOAD} download -f pxe
@@ -19,15 +21,15 @@ fi
 ${DOCKER} run -i --rm ${IMAGE_BUTANE} -p -s <fcos-config.yml | tee coreos/fcos-config.ign
 
 
-if [[ $("${SUDO}" virsh net-list --all | grep -c "${NETWORK}") == "0" ]]; then
-  ${SUDO} virsh net-define "${NETWORK}.xml"
-  ${SUDO} virsh net-start "${NETWORK}"
-  ${SUDO} virsh net-autostart "${NETWORK}"
+if [[ $("${SUDO}" virsh --connect="${CONNECTION}" net-list --all | grep -c "${NETWORK}") == "0" ]]; then
+  ${SUDO} virsh --connect="${CONNECTION}" net-define "${NETWORK}.xml"
+  ${SUDO} virsh --connect="${CONNECTION}" net-start "${NETWORK}"
+  ${SUDO} virsh --connect="${CONNECTION}" net-autostart "${NETWORK}"
 fi
 
 ${SUDO} ${DOCKER} run -d --rm --name http-server -p 192.168.123.1:80:8080  -v "${PWD}/coreos:/coreos:z" -w /coreos docker.io/python:alpine3.17 python -m http.server 8080
 
-${SUDO}  virt-install --connect="qemu:///system" \
+${SUDO}  virt-install --connect="${CONNECTION}" \
   --name="${VM_NAME}" \
   --vcpus="${VCPUS}" \
   --memory="${RAM_MB}" \
